90f2d7afbf5c50e3ec1282481ac6ebf52df30172
Liloker <55574496+Liloker@users.noreply.github.com>
2024-02-21 17:52:34 +0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -7,20 +7,10 @@ LISTEN_PORT = 5431
 
 @app.route('/api')
 def hello_world():
-    output('[-]:hello_world接口被调用了')
-    return 'Hello World!'
+    output('[-]:test api')
+    return 'test success!'
 
 
 if __name__ == '__main__':
 
     app.run(host=LISTEN_HOST, port=LISTEN_PORT, debug=True)
-
-
-
-
-
-
-
-
-
-
